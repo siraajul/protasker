4c0685dbce0cf477de376210c67419c3605f0684
siraajul <shirajulislamparvez@gmail.com>
2024-11-14 13:40:33 +0600

    Add:--> Header

---
+++ src/components/others/Header.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { setLocalStroage } from '../../utils/LocalStrorage';
+
+const Header = ({data, changeUser}) => {
+  const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    if (!data) {
+      setUsername('Admin');
+    } else {
+      setUsername(data.firstName);
+    }
+  }, [data]);
+
+  const logOut = () => {
+    localStorage.setItem("loggedInUser", "")
+    changeUser('')
+    // window.location.reload()
+  }
+
+  return (
+    <div className='flex items-end justify-between '>
+      <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{username} 👋</span> </h1>
+      <button onClick={logOut} className='bg-red-600 px-5 py-2 rounded-sm  text-lg font-medium'>Log Out</button>
+    </div>
+  );
+}
+
+export default Header;
